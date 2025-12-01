c8089b685779dec986623d2dfa2eb6f49a33ada1
Hashan Athurugiriya <hashan.athurugiriya@gmail.com>
2025-12-01 17:00:36 +0530

    animate cocktails section

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -5,7 +5,18 @@ import { cocktailLists, mockTailLists } from "../constants";
 
 const Cocktails = () => {
   useGSAP(() => {
-    const parallaxTimeline = gsap.timeline({})
+    const parallaxTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#cocktails",
+        start: "top 30%",
+        end: "bottom 80%",
+        scrub: true,
+      },
+    });
+
+    parallaxTimeline
+      .from("#c-left-leaf", { x: -100, y: 100 })
+      .from("#c-right-leaf", { x: 100, y: 100 });
   }, []);
 
   return (
