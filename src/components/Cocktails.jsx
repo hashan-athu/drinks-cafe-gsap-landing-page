e7d2b3ea25d9764723533019a263eb0950cdd08f
Hashan Athurugiriya <hashan.athurugiriya@gmail.com>
2025-12-01 16:50:49 +0530

    add content to the cokctails section

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { use } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { cocktailLists, mockTailLists } from "../constants";
 
 const Cocktails = () => {
-  return (
-    <div>Cocktails</div>
-  )
-}
+  useGSAP(() => {
+    const parallaxTimeline = gsap.timeline({})
+  }, []);
 
-export default Cocktails
+  return (
+    <section id="cocktails" className="noisy">
+      {/* Leaf Images */}
+      <img
+        src="/images/cocktail-left-leaf.png"
+        alt="left-leaf"
+        id="c-left-leaf"
+      />
+      <img
+        src="/images/cocktail-right-leaf.png"
+        alt="right-leaf"
+        id="c-right-leaf"
+      />
+
+      <div className="list">
+        {/* Cocktails List */}
+        <div className="popular">
+          <h2>Most Popular Cocktails:</h2>
+
+          <ul>
+            {cocktailLists.map(({ name, country, detail, price }) => (
+              <li key={name}>
+                <div className="md:me-28">
+                  <h3>{name}</h3>
+                  <p>
+                    {country} | {detail}
+                  </p>
+                </div>
+                <span>- {price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Mocktails List */}
+        <div className="loved">
+          <h2>Most Loved Mocktails:</h2>
+
+          <ul>
+            {mockTailLists.map(({ name, country, detail, price }) => (
+              <li key={name}>
+                <div className="me-28">
+                  <h3>{name}</h3>
+                  <p>
+                    {country} | {detail}
+                  </p>
+                </div>
+                <span>- {price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Cocktails;
