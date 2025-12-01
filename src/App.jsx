19864278c9d4d32fa92d05649781e94b211f48ee
Hashan Athurugiriya <hashan.athurugiriya@gmail.com>
2025-12-01 17:09:49 +0530

    Fix Navbar mispelling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 
