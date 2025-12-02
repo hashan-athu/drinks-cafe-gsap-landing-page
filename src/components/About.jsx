ddf6d66dbcef03816c9f9a254cec1f71fab5e63b
Hashan Athurugiriya <hashan.athurugiriya@gmail.com>
2025-12-02 16:58:00 +0530

    animate about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,35 @@
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+import { gsap } from "gsap";
 import React from "react";
 
 const About = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create("#about h2", { type: "words" });
+
+    const scrollTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#about",
+        start: "top center",
+      },
+    });
+
+    scrollTimeline
+      .from(titleSplit.words, {
+        opacity: 0,
+        duration: 1,
+        yPercent: 100,
+        ease: "expo.out",
+        stagger: 0.02,
+      })
+      .from('.top-grid div, .bottom-grid div', {
+        opacity: 0,
+        duration: 1,
+        ease: 'power1.inOut',
+        stagger: 0.04
+      }, '-=0.5');
+  });
+
   return (
     <div id="about">
       <div className="mb-16 md:px-0 px-5">
