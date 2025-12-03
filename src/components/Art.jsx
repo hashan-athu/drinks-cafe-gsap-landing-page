35cac0f590f4ec96c1d4cc9a065b6019bfcf2138
Hashan Athurugiriya <hashan.athurugiriya@gmail.com>
2025-12-03 09:37:26 +0530

    add the art content

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import { goodLists, featureLists } from "../constants/";
 
 const Art = () => {
   return (
-    <div>Art</div>
-  )
-}
+    <div id="art">
+      <div className="container mx-auto h-full pt-20">
+        <h2 className="will-fade">The ART</h2>
 
-export default Art
+        <div className="content">
+          {/* Good List */}
+          <ul className="space-y-4 will-fade">
+            {goodLists.map((feature, index) => (
+              <li key={index} className="flex items-center gap-2">
+                <img src="/images/check.png" alt="check" />
+                <p>{feature}</p>
+              </li>
+            ))}
+          </ul>
+
+          {/* Image */}
+          <div className="cocktail-img">
+            <img
+              src="/images/under-img.jpg"
+              alt="cocktail"
+              className="abs-center masked-img size-full object-contain"
+            />
+          </div>
+
+          {/* Features List */}
+          <ul className="space-y-4 will-fade">
+            {featureLists.map((feature, index) => (
+              <li key={index} className="flex items-center justify-start gap-2">
+                <img src="/images/check.png" alt="check" />
+                <p className="md:w-fit w-60">{feature}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Bottom Content */}
+        <div className="masked-container">
+          <h2 className="will-fade">Sip-Worthy Perfection</h2>
+          <div id="masked-content">
+            <h3>Made with Craft, Poured with Passion</h3>
+            <p>
+              This isn't just a drink. It's a carefully crafted moment made just
+              for you.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Art;
