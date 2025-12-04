d143cecd078241ccc8a015b26334d6793e1e7d27
Hashan Athurugiriya <hashan.athurugiriya@gmail.com>
2025-12-04 13:14:14 +0530

    add contact section content

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { use, useRef } from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
 import { sliderLists } from "../constants";
 import { useGSAP } from "@gsap/react";
